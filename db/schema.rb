4fa5ddea4135a4bd72aa8a52b9d789017f906fba
railsfactory-pavan <pavanrf07@gmail.com>
2019-02-20 19:42:06 +0530

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_102846) do
+ActiveRecord::Schema.define(version: 2019_02_20_134619) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "location"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
